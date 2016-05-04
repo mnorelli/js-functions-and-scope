37476ea99083a597eb83d3c0037228a3424471b8
mnorelli <mnorelli360@gmail.com>
2016-05-04 16:34:57 -0700

    add solutions and responses for three questions

--- starter-code/independent-practice.js
+++ starter-code/independent-practice.js
@@ -5,7 +5,15 @@
 */
 
 // your code here
-
+var words = ["hello", "what", "is", "up", "dude"]
+function lengths(list) {
+  var newList = []
+  list.forEach(function(i){
+    newList.push(i.length)
+  })
+  return newList;
+}
+console.log("Question 1: " + lengths(words));
 /*
   2. Write a Javascript function called `transmogrifier`
   This function should accept three arguments, which you can assume will be numbers.
@@ -15,10 +23,24 @@
 */
 
 // your code here
-
+function transmogrifier(num1, num2, num3) {
+  return Math.pow((num1*num2),num3);
+}
+console.log("Question 2: Transmogrifying 8, -2, and 3 gives: " + transmogrifier(8,-2,3))
 /*
   3. Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words (not letters) reversed. Don't worry
   about punctuation. I.e. "hello world" should become "world hello".
 */
 
 // your code here
+var phrase = "Good day, old bean."
+function wordReverse(str) {
+  strArray = phrase.split(' ')
+  var ans3 = ""
+  for (i=strArray.length;i>=0;i--) {
+    if (i!=strArray.length) {
+        ans3 = ans3 + " " + strArray[i]}
+  }
+  return ans3
+}
+console.log("Question 3: reversing "+ phrase + " gives " + wordReverse(phrase))
